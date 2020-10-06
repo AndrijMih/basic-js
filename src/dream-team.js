571f516519311ad51215a8d50f9a2b55d31c2aea
andrijmih <an12dre34@rambler.ru>
2020-10-06 22:00:15 +0300

    feat: add Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length === 0) {
+    return false;
+  }
+
+  let dream = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] !== 'string') {
+      continue;
+    }
+    dream = dream + members[i].trim().toUpperCase()[0];
+  }
+  return dream.split('').sort().join('');
+
 };
